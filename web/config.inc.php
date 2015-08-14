cb8620e3e3bb11c0b24d2e2a8d8c6a8936a383f3
Dave Tarrant <davetaz@theodi.org>
2015-08-14 17:11:56 +0100

    race condition updates

--- web/config.inc.php
+++ web/config.inc.php
@@ -5,5 +5,5 @@
 	$collection = getenv("MONGOLAB_COLLECTION");
 	$mandrill_key = getenv("MANDRILL_KEY");
 	$eLearning_prefix = getenv("ELEARNING_PREFIX");
-	$mail_lock = false;
+	$mail_lock = getenv("MAIL_LOCK");
 ?>
--- web/sendMail.php
+++ web/sendMail.php
@@ -5,7 +5,7 @@ include_once 'config.inc.php';
 
 function findEmails() {
    global $connection_url, $db_name, $collection, $mail_lock;
-   $mail_lock = true;
+   putenv("MAIL_LOCK=true");
    try {
 	 // create the mongo connection object
 	$m = new MongoClient($connection_url);
@@ -34,7 +34,7 @@ function findEmails() {
 //	return false;
 	syslog(LOG_ERR,'Error: ' . $e->getMessage());
    }
-   $mail_lock = false;
+   putenv("MAIL_LOCK=false");
 }
 
 function markDone($id) {
