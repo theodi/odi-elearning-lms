96d9c8e970f13e545300f202a75f08b88a4e30ec
Dave Tarrant <davetaz@theodi.org>
2016-01-14 16:28:55 +0000

    Commit test api

--- web/api/archive.php
+++ web/api/archive.php
@@ -1,12 +1,10 @@
 <?php
-echo "DONE";
-exit();
-
 $location = "api/archive.php";
 $path = "../";
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 include('_includes/api-header.php');
-
+echo "DONE";
+exit();
 include('_includes/functions.php');
 
 $count = archive_empty_profiles();
