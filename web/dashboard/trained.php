19250c7be7785cdd3834482cc0e1cde13943f27e
Dave Tarrant <davetaz@theodi.org>
2016-01-27 16:03:46 +0000

    maybe

--- web/dashboard/trained.php
+++ web/dashboard/trained.php
@@ -7,9 +7,47 @@
 	$data = get_data_from_collection($collection);
 	$courses = getCoursesData();
 	foreach ($data as $user) {
-		print_r($user);
-		exit(1);
+		$complete_modules = getCompleteModuleCount($user,$courses);
+		if ($complete_modules > 0) {
+			$people_trained++;
+			$complete[$complete_modules]++;
+		}
 	}
-	
+	echo $people_trained;
+	print_r($complete);
+
+function getCompleteModuleCount($user,$courses) {
+	$complete = 0;
+	foreach($user as $key => $data) {
+                $key = str_replace("．",".",$key);
+                if (strpos($key,"_cmi.suspend_data") !== false) {
+                        $course = substr($key,0,strpos($key,"_cmi"));
+                        $progress = $data;
+                        if ($courses[$course]) {
+                                $courses[$course]["progress"] = getProgress($courses[$course],$progress);
+                                if ($courses[$course]["progress"] > 99) {
+                                        $complete++;
+                                }
+                        }
+                }
+        }
+	return $complete;
+}
+
+function getProgress($course,$progress) {
+        $spoor = json_decode($progress,true);
+        $progress = $spoor["spoor"];
+        if ($progress["_isAssessmentPassed"] > 0 || $progress["_isCourseComplete"] > 0) {
+                $progress["completion"] = str_replace("0","1",$progress["completion"]);
+                return 100;
+        }
+        $total = strlen($progress["completion"]);
+        $sub = substr_count($progress["completion"],0);
+        $complete = round(($sub / $total) * 100);
+        return $complete;       
+}
+
+
+
 	include('_includes/footer.html');
 ?>
